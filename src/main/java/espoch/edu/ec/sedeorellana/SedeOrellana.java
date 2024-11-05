69bdb25aa8073a355a72d12a8698d82247603cdd
Jost1nVx7 <jostinjd2002@gmail.com>
2024-11-05 15:07:26 -0500

    esperanza

---
+++ src/main/java/espoch/edu/ec/sedeorellana/SedeOrellana.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.sedeorellana;
+
+/**
+ *
+ * @author Global Technology Ec
+ */
+public class SedeOrellana {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
